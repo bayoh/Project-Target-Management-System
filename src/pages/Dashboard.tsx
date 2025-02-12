bc8610334a905d6bdfaa89575fb01d4c4871d402
bayoh <bayohi90@gmail.com>
2025-02-12 13:41:50 +0000

    move back targets

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -290,7 +290,7 @@ export function Dashboard() {
           </div>
         </div>
 
-        {/* <ProjectStats/> */}
+        <ProjectStats/>
 
         {/* Clusters and Pathways */}
         <div className="space-y-6">
@@ -361,7 +361,7 @@ export function Dashboard() {
           )}
         </div>
       </div>
-      <ProjectStats/>
+      {/* <ProjectStats/> */}
     </DashboardLayout>
   );
 }
